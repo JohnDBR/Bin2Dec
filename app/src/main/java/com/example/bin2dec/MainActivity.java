e42c40e0f847613f3239c5373287bcb40176ffe0
krthr <twilson@uninorte.edu.co>
2020-02-04 19:39:25 -0500

    toolbar done

--- app/src/main/java/com/example/bin2dec/MainActivity.java
+++ app/src/main/java/com/example/bin2dec/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 
